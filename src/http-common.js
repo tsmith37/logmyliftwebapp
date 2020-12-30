61f38b99f26d56b7b211ee730087de530f18349e
tommy <tsmith37@mix.wvu.edu>
2020-12-30 14:15:07 -0600

    logging API path as its not getting set

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 var base_url = (process.env.API_URL ? process.env.API_URL : "http://localhost:8080/api");
+console.log(base_url);
 export default axios.create({
 	baseURL: base_url,
 	headers: {
