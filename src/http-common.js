f442f66c98f8060fa2db34376dc320ff8f7a0e09
tommy <tsmith37@mix.wvu.edu>
2020-12-30 14:06:21 -0600

    parameterizing API path

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
+var base_url = (process.env.API_URL ? process.env.API_URL : "http://localhost:8080/api");
 export default axios.create({
-	baseURL: "http://localhost:8080/api",
+	baseURL: base_url,
 	headers: {
 		"Content-type": "application/json"
 	}
